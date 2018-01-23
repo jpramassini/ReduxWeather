f943e6d20185e36a251fb85d9eb7c7492aeb5ad4
jpramassini <jpramassini@gmail.com>
2018-01-23 12:52:22 -0500

    Created Reducer to handle API call result/promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,6 +10,8 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`
   const request = Axios.get(url);
 
+  console.log('Request:', request);
+
   return{
     type: FETCH_WEATHER,
     payload: request
